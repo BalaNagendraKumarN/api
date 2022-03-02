3a6e37f5557747d94cfa6b8e30f8996ce647d19a
BalaNagendraKumarN <balankn@dckap.com>
2022-03-02 22:43:17 +0530

    first commit

--- api/amount.php
+++ api/amount.php
@@ -1,7 +1,10 @@
 <?php
-header("Access-Control-Allow-Origin: $_SERVER['HTTP_HOST']");
-header("Access-Control-Allow-Headers: Content-Type, origin");
+//header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+header("Content-Type: application/json; charset=UTF-8");
 
 
 // Only allow POST requests
