6bf7b08b5138fa11f9d1d20a011da2da5219df65
BalaNagendraKumarN <balankn@dckap.com>
2022-03-07 08:57:54 +0530

    new

--- api/amount.php
+++ api/amount.php
@@ -1,10 +1,22 @@
 <?php
 //header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-header('Access-Control-Max-Age: 1000');
-header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-header("Content-Type: application/json; charset=UTF-8");
+// header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+// header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+// header('Access-Control-Max-Age: 1000');
+// header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+// header("Content-Type: application/json; charset=UTF-8");
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization, x-xsrf-token");
+header('Content-Type: application/json');
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method == "OPTIONS") {
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization,x-xsrf-token");
+header("HTTP/1.1 200 OK");
+die();
+}
 
 
 // Only allow POST requests
@@ -23,13 +35,13 @@ $body = file_get_contents("php://input");
 
 $array = json_decode($body,TRUE);
 
-if($array['pincode'] == '123122'){
+if($array['pincode'] == '600011'){
     $response['message'] = 'success';
     $response['price'] = '11';
-}elseif($array['pincode'] == '123123'){
+}elseif($array['pincode'] == '600039'){
     $response['message'] = 'success';
     $response['price'] = '12';
-}elseif($array['pincode'] == '123124'){
+}elseif($array['pincode'] == '600012'){
     $response['message'] = 'success';
     $response['price'] = '10';
 }else{
