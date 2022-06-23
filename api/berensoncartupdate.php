8d84fd8e33247b5309c270d08107955123af2c05
BalaNagendraKumarN <99191922+BalaNagendraKumarN@users.noreply.github.com>
2022-06-23 16:37:06 +0530

    Update berensoncartupdate.php

--- api/berensoncartupdate.php
+++ api/berensoncartupdate.php
@@ -12,18 +12,6 @@ header("HTTP/1.1 200 OK");
 die();
 }
 
-
-// Only allow POST requests
-if (strtoupper($_SERVER['REQUEST_METHOD']) != 'POST') {
-  throw new Exception('Only POST requests are allowed');
-}
-
-// Make sure Content-Type is application/json 
-$content_type = isset($_SERVER['CONTENT_TYPE']) ? $_SERVER['CONTENT_TYPE'] : '';
-if (stripos($content_type, 'application/json') === false) {
-  throw new Exception('Content-Type must be application/json');
-}
-
 // Read the input stream
 $body = file_get_contents("php://input");
 
@@ -71,7 +59,7 @@ curl_close($curl);
 if ($err) {
   echo "cURL Error #:" . $err;
 } else {
-  echo $response;
+  echo "success";
 }
 
 
